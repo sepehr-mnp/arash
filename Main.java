42e15cf0b43d7814b552ae7e731a62402fb7e424
sepehrmnp <sepehr.mirnasrollahi@aut.ac.ir>
2022-12-19 12:13:22 -0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,124 +2,50 @@ package org.example;
 
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
-// Server class
-class Server {
+// Client class
+class Client {
+
+    // driver code
     public static void main(String[] args)
     {
-        ServerSocket server = null;
+        // establish a connection by providing host and port
+        // number
+        try (Socket socket = new Socket("localhost", 1234)) {
 
-        try {
+            // writing to server
+            PrintWriter out = new PrintWriter(
+                    socket.getOutputStream(), true);
 
-            // server is listening on port 1234
-            server = new ServerSocket(1234);
-            server.setReuseAddress(true);
+            // reading from server
+            BufferedReader in
+                    = new BufferedReader(new InputStreamReader(
+                    socket.getInputStream()));
 
-            // running infinite loop for getting
-            // client request
-            while (true) {
+            // object of scanner class
+            Scanner sc = new Scanner(System.in);
+            String line = null;
 
-                // socket object to receive incoming client
-                // requests
-                Socket client = server.accept();
+            while (!"exit".equalsIgnoreCase(line)) {
 
-                // Displaying that new client is connected
-                // to server
-                System.out.println("New client connected"
-                        + client.getInetAddress()
-                        .getHostAddress());
+                // reading from user
+                line = sc.nextLine();
 
-                // create a new thread object
-                ClientHandler clientSock
-                        = new ClientHandler(client);
+                // sending the user input to server
+                out.println(line);
+                out.flush();
 
-                // This thread will handle the client
-                // separately
-                new Thread(clientSock).start();
+                // displaying server reply
+                System.out.println("Server replied "
+                        + in.readLine());
             }
+
+            // closing the scanner object
+            sc.close();
         }
         catch (IOException e) {
             e.printStackTrace();
-        }
-        finally {
-            if (server != null) {
-                try {
-                    server.close();
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    // ClientHandler class
-    private static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
-
-        // Constructor
-        public ClientHandler(Socket socket)
-        {
-            this.clientSocket = socket;
-        }
-
-        public void run()
-        {
-            PrintWriter out = null;
-            BufferedReader in = null;
-            try {
-
-                // get the outputstream of client
-                out = new PrintWriter(
-                        clientSocket.getOutputStream(), true);
-
-                // get the inputstream of client
-                in = new BufferedReader(
-                        new InputStreamReader(
-                                clientSocket.getInputStream()));
-
-                String line;
-                while ((line = in.readLine()) != null) {
-
-                    System.out.printf(
-                            " Sent from the client: %s\n",
-                            line);
-                    if(line.equals("Hi there!"))
-                    {
-                        out.println("Hi Client.");
-                    }else if(line.equals("How are you?")){
-                        out.println("Still working , Thanks!");
-                    }else if(line.equals("My name is NAME")){
-                        out.println("Nice to meet you NAME, my name is WHATEVER");
-                    }else if(line.equals("Nice to meet you too, I Have to go, How can I turn you off?")){
-                        out.println("Aww! could you please stay more, I just turned on.");
-                    }else if(line.equals("I'm sorry I can't!")){
-                        out.println("Okay, You can turn Me off by sending EXIT message.");
-                    }else if(line.equals("EXIT")){
-                        out.println("Bye friend. Sever turned off...");
-                        break;
-                    }else{
-                        out.println("say again! couldn't understand.");
-                    }
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            finally {
-                try {
-                    if (out != null) {
-                        out.close();
-                    }
-                    if (in != null) {
-                        in.close();
-                        clientSocket.close();
-                    }
-                }
-                catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
